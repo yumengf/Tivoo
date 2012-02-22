e12c6ceea8f4d3b95bdeeef65c218d7b08de126e
Fred Yinan Xie <xie.yn1018@gmail.com>
2012-02-22 15:04:12 -0500

    Change it to interface

--- src/Parser.java
+++ src/Parser.java
@@ -7,9 +7,6 @@ import org.jdom.JDOMException;
 /*
  * Parser is an abstract super class for all subclass Parsers for individual .xml files
  */
-public abstract class Parser {
-	
+public abstract interface Parser {
 	public abstract ArrayList<CalendarEvent> parse(Element rootNode) throws JDOMException, IOException;
-	
-	
 }
