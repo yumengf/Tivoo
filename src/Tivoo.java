0310ebeefb99d381685286b97c215c495d79fda8
kaitlynduke <yumengf@gmail.com>
2012-02-23 17:54:51 -0500

    resolve conflict

--- src/Tivoo.java
+++ src/Tivoo.java
@@ -1,6 +1,6 @@
 import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
 import java.util.*;
-
 import org.jdom.Element;
 import org.jdom.JDOMException;
 
@@ -9,25 +9,28 @@ import org.jdom.JDOMException;
  * and parses, filters and outputs an HTML file that shows relevant information
  */
 public class Tivoo {
-	public static void main(String args[]) throws ClassNotFoundException, InstantiationException, IllegalAccessException {
+	public static void main(String args[]) throws ClassNotFoundException, InstantiationException, IllegalAccessException, Exception, IllegalArgumentException, NoSuchMethodException, InvocationTargetException {
 		try {
-			String file = "src/NFLcalender.xml";
+			String file = "src/NFLcalendar.xml";
 			ParserFactory factory = new ParserFactory();
 			Parser parser = factory.getParser(file);
 			Element rootNode = factory.getMyRoot();
-			
+
 			ArrayList<CalendarEvent> list = parser.parse(rootNode);
-			Process processor = new Process(list);
-			list = processor.keyword("49ers");
-			
+
+			Process processor = new Process();
+			ArrayList<Object> l = new ArrayList<Object>();
+			l.add(new String ("NFL"));
+			list = processor.filter("keyword", l, list);
+
 			Output output = new Output(list);
 			output.outputFile();
-			 
+
 		} catch (JDOMException e) {
 			e.printStackTrace();
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		
+
 	}
 }
