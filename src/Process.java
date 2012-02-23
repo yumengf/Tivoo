00eca25f8e46932619ee85b54980a50393495f60
Fred Yinan Xie <xie.yn1018@gmail.com>
2012-02-22 21:38:01 -0500

    update

--- src/Process.java
+++ src/Process.java
@@ -1,42 +1,23 @@
+import java.lang.reflect.InvocationTargetException;
 import java.util.ArrayList;
-import org.joda.time.DateTime;
 
 public class Process {
-
-   private ArrayList<CalendarEvent> myEvents = new ArrayList<CalendarEvent>();
-   public Process (ArrayList<CalendarEvent> inputEvents){
-           myEvents = inputEvents;
-   }
-
-   /*
-    * Returns an Arraylist of events which contains all the events
-    * that contain a "keyword"
-    */
-   public ArrayList<CalendarEvent> keyword(String keyword){
-       ArrayList<CalendarEvent> myEventsToReturn = new ArrayList<CalendarEvent>();
-       for (CalendarEvent currentEvent: myEvents){
-           // if the event has an index != -1. it means it contains the word
-           // and therefore adds it to the list
-           if (currentEvent.hasKeyWord(keyword)){
-                   myEventsToReturn.add(currentEvent);
-           }
-       }
-       return myEventsToReturn;
-   }
-
-   /*
-    * Returns an Arraylist of events which contains all the events
-    * within a given time frame
-    */
-   public ArrayList<CalendarEvent> timeFrame(DateTime start, DateTime finish){
-       ArrayList<CalendarEvent> myEventsCopy = new ArrayList<CalendarEvent>();
-       
-       for (CalendarEvent currentEvent: myEvents){
-           //Start Date is greater than or equal to start date
-           if(currentEvent.isInTimeFrame(start, finish)){
-        	   myEventsCopy.add(currentEvent);
-           }
-       }
-       return myEventsCopy;
-   }
+	// creates a new filter class
+	FilterFactory myFilterFactory = new FilterFactory();
+	
+	public Process() throws ClassNotFoundException{
+		try {
+	        myFilterFactory = new FilterFactory();
+        } catch (Exception e) {
+	        e.printStackTrace();
+        } 
+	}
+	
+	public ArrayList<CalendarEvent> filter (String myCommand, ArrayList<Object> myParameters, ArrayList<CalendarEvent> eventsToFilter) throws SecurityException, IllegalArgumentException, InstantiationException, IllegalAccessException, NoSuchMethodException, InvocationTargetException{		
+		// uses the filter factory to get the correct event filter using the command line 
+		Filter myFilter = myFilterFactory.getFilter(myCommand);		
+		// calls the filter method in the specified class and returns an arraylist of the events after being filtered
+		return myFilter.filter(myParameters, eventsToFilter); 	
+	}
 }
+
