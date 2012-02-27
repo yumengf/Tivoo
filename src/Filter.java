84bffab3663ff83ce1603eec65eb449c9a4cb115
kaitlynduke <yumengf@gmail.com>
2012-02-26 22:42:32 -0500

    added package, code unchanged from 9pm

--- src/Filter.java
+++
@@ -1,7 +0,0 @@
-import java.util.ArrayList;
-
-public interface Filter {
-	public abstract String getCommandName(); 
-	public abstract ArrayList<CalendarEvent> filter(ArrayList<Object> parameters, ArrayList<CalendarEvent> events); 
-	
-}
