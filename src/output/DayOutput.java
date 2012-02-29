d520e5f20ca93590f859581c9360d8148e6eda78
unknown <fred@fred-PC.(none)>
2012-02-29 10:26:46 -0500

    update

--- src/output/DayOutput.java
+++ src/output/DayOutput.java
@@ -3,6 +3,8 @@ package output;
 import java.util.ArrayList;
 import java.util.List;
 
+import Tivoo.CalendarEvent;
+
 import com.hp.gagawa.java.elements.A;
 import com.hp.gagawa.java.elements.Body;
 import com.hp.gagawa.java.elements.H1;
@@ -14,46 +16,46 @@ import com.hp.gagawa.java.elements.Td;
 import com.hp.gagawa.java.elements.Tr;
 import com.hp.gagawa.java.elements.Ul;
 
-import input.CalendarEvent;
-
 /*
  *DayOutput class receive list of CalendarEvent objects with information of eventName, date, location, 
  *startTime and endTime, and then output the information into a html file with specific information linked to each event.
  */
 public class DayOutput extends Output {
 	private List<CalendarEvent> myCalendar;
-	private static final String[] Weekday = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
-	
+	private static final String[] Weekday = {"Monday", "Tuesday",
+			"Wednesday", "Thursday", "Friday", "Saturday", "Sunday"};
+
 	public DayOutput(ArrayList<CalendarEvent> cal) {
 		myCalendar = cal;
 	}
-	
+
 	public void outputFile(String string) {
 		Html html = new Html();
 		Body body = new Body();
 		html.appendChild(body);
 		body = constructDayFrame(body);
-		Table table = new Table().setBgcolor("grey").setBorder("2").setAlign("center");
+		Table table = new Table().setBgcolor("grey").setBorder("2")
+				.setAlign("center");
 		body.appendChild(table);
-		
-		table = addEventList(table);			//Add Event list to each day
-		
+		table = addEventList(table); // Add Event list to each day
+
 		writeInFile(html, "Output/DayOutput.htm");
 	}
-	
+
 	/*
-	 * Takes in a Body object, initialize the title of day in the week
-	 * Used in outputFile method.
-	 */	
+	 * Takes in a Body object, initialize the title of day in the week Used in
+	 * outputFile method.
+	 */
 	private Body constructDayFrame(Body body) {
 		H1 title = new H1().setAlign("center");
 		body.appendChild(title);
 		int i = myCalendar.get(0).startDayOfWeek();
-		title.appendText(Weekday[i]).setAlign("center");	
+		title.appendText(Weekday[i-1]).setAlign("center");
 		return body;
 	}
+
 	/*
-	 * Takes in a Table object, append Events within a day, return the table. 
+	 * Takes in a Table object, append Events within a day, return the table.
 	 * Used in outputFile method.
 	 */
 	private Table addEventList(Table table) {
@@ -61,15 +63,16 @@ public class DayOutput extends Output {
 		table.appendChild(tr);
 		Td td = new Td().setAlign("center");
 		tr.appendChild(td);
-		Ul ul = new Ul();	
+		Ul ul = new Ul();
 		td.appendChild(ul);
 		Li li = new Li();
-		ul.appendChild(li);	
-		
-		for(int i = 0; i < myCalendar.size(); i++) {
+		ul.appendChild(li);
+
+		for (int i = 0; i < myCalendar.size(); i++) {
 			CalendarEvent c = myCalendar.get(i);
-			A link = new A().setHref(eventFile("Day", i, 1, c)).setTarget("_blank");
-			H5 head = new H5().appendText(c.getMyName()).setAlign("center");				
+			A link = new A().setHref(eventFile("Day", i, 1, c)).setTarget(
+					"_blank");
+			H5 head = new H5().appendText(c.getMyName()).setAlign("center");
 			link.appendChild(head);
 			li.appendChild(link);
 		}
